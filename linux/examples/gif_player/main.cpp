f0142cf834d4e2d59151de09df8cbeb37fc4e91d
Larry Bank <bitbank@pobox.com>
2025-12-16 18:17:19 +0000

    Updated example for Adafruit Pi_TFT as the output device

--- linux/examples/gif_player/main.cpp
+++ linux/examples/gif_player/main.cpp
@@ -8,7 +8,19 @@
 #include <time.h>
 AnimatedGIF gif;
 BB_SPI_LCD lcd;
-#define RADXA
+//#define RADXA
+//#define PARALLEL_LCD
+#define ADAFRUIT_PITFT
+
+#ifdef ADAFRUIT_PITFT
+#define LCD_TYPE LCD_ILI9341
+#define DC_PIN 25
+#define RESET_PIN 27
+#define CS_PIN 8
+#define LED_PIN -1
+#define SPI_NUM 0
+#define GPIO_CHIP 0
+#endif // ADAFRUIT_PITFT
 
 #ifdef RADXA
 #define LCD_TYPE LCD_ST7789
@@ -18,7 +30,8 @@ BB_SPI_LCD lcd;
 #define LED_PIN 56
 #define GPIO_CHIP 4
 #define SPI_NUM 12
-#else
+#endif
+#ifdef PARALLEL_LCD
 // definitions for parallel 3.5" Kumon LCD shield
 uint8_t u8DataPins[8] = {14,15,16,17,18,19,20,21};
 #define LCD_TYPE LCD_ILI9488
@@ -28,7 +41,7 @@ uint8_t u8DataPins[8] = {14,15,16,17,18,19,20,21};
 #define CS_PIN 22
 #define DC_PIN 4
 #define BUS_WIDTH 8
-#endif // RADXA
+#endif // PARALLEL_LCD
 
 int MilliTime()
 {
@@ -51,14 +64,17 @@ int main(int argc, char *argv[])
       printf("bb_spi_lcd gif_player\nUsage: gif_player <file> <optional loop count>\n");
       return -1;
   }
-#ifdef RADXA
-  lcd.begin(LCD_TYPE, FLAGS_NONE, 30000000, CS_PIN, DC_PIN, RESET_PIN, LED_PIN, GPIO_CHIP, SPI_NUM, 0);
+#ifndef PARALLEL_LCD
+  lcd.begin(LCD_TYPE, FLAGS_NONE, 62500000, CS_PIN, DC_PIN, RESET_PIN, LED_PIN, GPIO_CHIP, SPI_NUM, 0);
+//    lcd.beginQSPI(LCD_ST77916B, 0, 17, 20, 13, 14, 15, 16, 18, 3);
 #else
   lcd.beginParallel(LCD_TYPE, FLAGS_SWAP_RB, RESET_PIN, RD_PIN, WR_PIN, CS_PIN
 , DC_PIN, BUS_WIDTH, u8DataPins, 18); // last parameter is the delay cycles for parallel data output
-#endif // RADXA
-  lcd.setRotation(90);
-  lcd.fillScreen(0);
+#endif // PARALLEL_LCD
+   if (lcd.width() < lcd.height()) {
+       lcd.setRotation(90);
+   }
+  lcd.fillScreen(TFT_BLACK);
 
   if (argc == 3) iLoops = atoi(argv[2]);
 
@@ -75,11 +91,11 @@ int main(int argc, char *argv[])
       iFrame = 0;
       iTime = MilliTime();
       for (int i=0; i<iLoops; i++) {
-          while (gif.playFrame(false, NULL, NULL)) {
+          int iDelay; // Frame delay in milliseconds
+          while (gif.playFrame(false, &iDelay, NULL)) {
 	      lcd.setAddrWindow(0,0,w,h);
-	      //i = MilliTime();
 	      lcd.pushPixels((uint16_t *)pStart, w * h);
-	      //i = MilliTime() - i;
+              usleep(iDelay * 1000); // delay between frames
 	      iFrame++;
           } // while decoding frames
 	  gif.reset();
